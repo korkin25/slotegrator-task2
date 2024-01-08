c56b94699fa75a4a60d9e9f43ac21f5c457aa231
korkin25 <eugeny.kuzakov@gmail.com>
2024-01-08 21:45:42 +0800

    http header is corrected/added.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+  res.statusCode = 200;
   res.end('Тестовое задание выполнено успешно!\n');
 });
 const port = 3000;
